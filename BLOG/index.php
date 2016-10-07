bd1b49dfcb6e9307d6da701511df954460739cfd
Fryntiz <fryntiz@gmail.com>
2016-10-07 17:16:36 +0200

    HTML básico para blog

---
+++ BLOG/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<HTML lang="es">
+<HEAD>
+	<TITLE>Nombre</TITLE>
+	<META charset="utf-8"/>
+	<META name="description" content="Plantilla de Ejemplo"/>
+	<META name="keywords" content="ejemplo, plantilla, programación, Raúl Caro Pastorino, Fryntiz"/>
+	<META name="author" content="Raúl Caro Pastorino"/>
+	<LINK rel="shortcut icon" href="./images/favicon.png"/>
+	<LINK rel="stylesheet" href="./CSS/styles.css"/>
+	<SCRIPT src="JS/scripts.js"></SCRIPT>
+</HEAD>
+
+<BODY>
+	<H1 id="titulo1">Título de la página</H1>
+	<H2 id="titulo2">Sección de que se trata</H2>
+	<P>Descripción</P>
+	<DIV class="ejemplo">
+		Plantilla de ejemplo
+	</DIV>
+<?php
+
+?>
+</BODY>
+</HTML>
