e49475d3adfd3ce58dfe5012dd08fda5280bd0de
Fryntiz <fryntiz@gmail.com>
2017-02-15 16:53:59 +0100

    Añadido desde plantilla el contenido

---
+++ Hostal/contenido.php
@@ -0,0 +1,52 @@
+		<DIV id="cajaContenido">
+			<DIV id="cajaEntradas">
+				<ARTICLE>
+					<HEADER>
+						<H2>Título de la Sección</H2>
+						<P>Descripción de la sección</P>
+						<TIME datetime="2017-02-14T12:30:00+01:00"><SMALL>Creado el 14-02-2017 a las 12:30</SMALL></TIME>
+					</HEADER>
+
+					<SECTION>
+						<H3>Nombre del apartado 1</H3>
+						<P>Datos del apartado 1</P>
+					</SECTION>
+
+					<SECTION>
+						<H3>Nombre del apartado 2</H3>
+						<P>Datos del apartado 2 (Relacionado con el 1)</P>
+					</SECTION>
+				</ARTICLE>
+
+				<HR />
+
+				<ARTICLE>
+					<HEADER>
+						<H2>Título de la Sección</H2>
+						<P>Descripción de la sección</P>
+						<TIME datetime="2017-02-14T01:20:00+01:00"><SMALL>Creado el 14-02-2017 a las 01:20</SMALL></TIME>
+					</HEADER>
+
+					<SECTION>
+						<H3>Nombre del apartado 1 de otra sección</H3>
+						<P>Datos del apartado 1 de otra sección, tema no relacionado con el anterior</P>
+					</SECTION>
+
+					<SECTION>
+						<H3>Nombre del apartado 2 de la segunda sección</H3>
+						<P>Datos del apartado 2 (Relacionado con el 1 de la segunda sección)</P>
+					</SECTION>
+				</ARTICLE>
+			</DIV>
+
+			<ASIDE>
+				<H4>Menú de navegación</H4>
+				<UL>
+					<LI><A href="#" title="Link 1">link 1</A></LI>
+					<LI><A href="#" title="Link 1">link 2</A></LI>
+					<LI><A href="#" title="Link 3">link 3</A></LI>
+					<LI><A href="#" title="Link 4">link 4</A></LI>
+				</UL>
+				<P>Descripción del apartado, datos, imagenes, banner, enlaces externos......</P>
+			</ASIDE>
+		</DIV>
