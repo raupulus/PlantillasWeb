92b660198aea4cfc17808228a38774c00bb09a9a
Fryntiz <fryntiz@gmail.com>
2017-02-15 16:40:38 +0100

    Añadidas etiquetas que son variables para cada página

--- Hostal/index.php
+++ Hostal/index.php
@@ -2,13 +2,26 @@
 <HTML lang="es">
 	<HEAD>
 		<TITLE>Titulo del sitio</TITLE>
-		<META charset="utf-8" />
-			<?php
+		<?php //Metaetiquetas globales importadas
 				include ('metaetiquetas-generales.php');
 			?>
+		<META name="keywords" content="etiquetas, clave, plantilla, web, fryntiz" />
+		<META name="subject" content="Descripción muy muy muy breve" />
+		<META name="description" content="Breve Resumen de la página WEB" />
+		<META name='revised' content='Tuesday, February 12th, 2017, 13:22 pm' /><!--Última revisión del sitio-->
+
+		<!--Metaetiquetas para mostrar WEB en Facebook-->
+		<META property='og:title' content='Titulo de la página' />
+		<META property='og:description' content='Descripción' />
+		<META property='og:image' content='http://www.dominioDelSitioWEB.es/img/logo.png' /><!--Miniatura que se mostrará junto al enlace-->
+
+		<!--Metaetiquetas para mostrar WEB en Twitter-->
+		<META name="twitter:title" content="Nombre de la publicación en twitter" />
+		<META name="twitter:description" content="Descripción de la página, máximo 200 caracteres" />
+
 	</HEAD>
 
 	<BODY>
-
+		<H1>Muestra</H1>
 	</BODY>
 </HTML>
