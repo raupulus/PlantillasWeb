40ab2b26abc67444743697f722d2c2e69e0d613b
Fryntiz <fryntiz@gmail.com>
2017-02-15 16:41:03 +0100

    Etiquetas metas que no van a variar

---
+++ Hostal/metaetiquetas-generales.php
@@ -0,0 +1,35 @@
+		<META charset="utf-8" />
+		<META name="author" lang="es" content="Raúl Caro Pastorino, tecnico@fryntiz.es" />
+		<META name="organization" content="fryntiz" />
+		<META name='copyright' content='Nombre de la compañia/organización' />
+
+		<META name='language' content='ES' />
+		<META name="robots" content="index, follow" /> <!--Indica si es indexada (index|noindex) y si se siguen enlaces (follow|nofollow)-->
+		<META name="viewport" content="width=device-width, initial-scale=1.0" /><!--Web responsive, no zoom en smartphone. Si no es responsive css quitar-->
+		<META name="google-site-verification" content="????-->Código de verificación google WEBMASTER" /><!--Verifica mediante Search consle, webmaster tools de google-->
+
+
+		<!--Metaetiquetas para mostrar WEB en Facebook-->
+		<META property='fb:admins' content='????????' /> <!--ID de los usuarios admin del sitio-->
+
+
+		<META property="og:locale" content="es_ES" />
+		<META property="og:locale:alternate" content="es_LA" />
+		<META property='og:url' content='http://www.dominioDelSitioWEB.es/' /><!--Dominio del sitio-->
+		<META property='og:site_name' content='Nombre del sitio' />
+		<META property='og:type' content='website' /> <!--Normalmente website y article-->
+
+
+		<!--Metaetiquetas para mostrar WEB en Twitter-->
+		<META name="twitter:card" content="summary" /> <!--Define el tipo de tarjeta (summary|photo|player)-->
+		<META name="twitter:site" content="@Organization" /><!--@username de la pagina en twitter-->
+		<META name="twitter:creator" content="@Author" /> <!--@username del creador o autor del contenido-->
+		<META name='twitter:image' content='http://midominio.com/logo.png' />
+
+
+		<!--Enlaces a favicon, CSS y JS-->
+		<LINK rel="shortcut icon" href="./img/favicon-32x32.png" />
+		<LINK rel="shortcut icon" href="./img/favicon-32x32.ico" />
+		<LINK rel="shortcut icon" href="./img/favicon-192x192.png" />
+		<LINK rel="stylesheet" href="./CSS/estilos.css" type="text/css" />
+		<SCRIPT src="./JS/scripts.js"></SCRIPT>
