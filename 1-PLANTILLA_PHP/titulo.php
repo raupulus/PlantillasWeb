07ac12b24c2eba9f4eae8a00474e6d66ed01c461
Fryntiz <fryntiz@gmail.com>
2017-02-15 17:08:34 +0100

    Título para plantilla en PHP

---
+++ 1-PLANTILLA_PHP/titulo.php
@@ -0,0 +1,9 @@
+		<HEADER id="cajaTitulo">
+			<FIGURE>
+				<IMG src="./img/Logotipo.png" alt="Mi Logo" title="Logotipo del sitio web" longdesc="http://www.miweb.es/index.html#descripcionweb" />
+				<FIGCAPTION>
+					<H1 id="titulo">Título del sitio con logo al fondo</H1>
+					<P id="descripcionweb">Descripción del sitio</P>
+				</FIGCAPTION>
+			</FIGURE>
+		</HEADER>
