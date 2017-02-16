3b7117d11cf66f9540e765d2b36bef2e41460e43
Fryntiz <fryntiz@gmail.com>
2017-02-16 11:48:29 +0100

    Añadida estructura a clientes.php

---
+++ Hostal/clientes.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<HTML lang="es">
+	<HEAD>
+		<TITLE>Administración Hotel</TITLE>
+		<?php //Metaetiquetas globales importadas
+				include ('metaetiquetas-generales.php');
+			?>
+		<META name='revised' content='Tuesday, February 13th, 2017, 13:22 pm' /><!--Última revisión del sitio-->
+	</HEAD>
+
+	<BODY>
+		<?php
+			include ('titulo.php');
+			include ('menu-principal.php');
+			echo	'<DIV id="cajaContenido">';
+			echo	'<DIV id="cajaEntradas">';
+
+			echo '</DIV>';
+			include ('aside.php');
+			echo	'</DIV>';
+			include ('footer.php');
+		?>
+	</BODY>
+</HTML>
