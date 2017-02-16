e9efa950ff76368bfae7ef41a539aeb68c65bd57
Fryntiz <fryntiz@gmail.com>
2017-02-16 09:49:33 +0100

    aside quitado de dentro de contenido

--- Hostal/contenido.php
+++ Hostal/contenido.php
@@ -35,15 +35,3 @@
 						<P>Datos del apartado 2 (Relacionado con el 1 de la segunda sección)</P>
 					</SECTION>
 				</ARTICLE>
-
-
-			<ASIDE>
-				<H4>Menú de navegación</H4>
-				<UL>
-					<LI><A href="#" title="Link 1">link 1</A></LI>
-					<LI><A href="#" title="Link 1">link 2</A></LI>
-					<LI><A href="#" title="Link 3">link 3</A></LI>
-					<LI><A href="#" title="Link 4">link 4</A></LI>
-				</UL>
-				<P>Descripción del apartado, datos, imagenes, banner, enlaces externos......</P>
-			</ASIDE>
