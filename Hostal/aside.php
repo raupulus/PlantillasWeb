d0f64d808c512862475ebcd41a0086ce5ed5a741
Fryntiz <fryntiz@gmail.com>
2017-02-16 11:25:32 +0100

    Añadido calendario al aside

--- Hostal/aside.php
+++ Hostal/aside.php
@@ -5,5 +5,6 @@
 					<LI><A href="#" title="Link 1">Ver habitaciones libres</A></LI>
 					<LI><A href="#" title="Link 3">Crear Reserva</A></LI>
 					<LI><A href="#" title="Link 4">Eliminar Reserva</A></LI>
+					<LI><A href="#" title="Link 5">Ver calendario</A></LI>
 				</UL>
 			</ASIDE>
