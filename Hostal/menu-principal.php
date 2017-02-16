dd339d801fb1a9086283ee9d5e4b331f1d020f6f
Fryntiz <fryntiz@gmail.com>
2017-02-16 11:23:41 +0100

    Añadida reservas al menú principal

--- Hostal/menu-principal.php
+++ Hostal/menu-principal.php
@@ -3,6 +3,6 @@
 				<LI><A href="#" title="Menú 1">Inicio</A></LI>
 				<LI><A href="#" title="Menú 2">Clientes</A></LI>
 				<LI><A href="#" title="Menú 3">Habitaciones</A></LI>
-				<LI><A href="#" title="Menú 4">Menú 4</A></LI>
+				<LI><A href="#" title="Menú 4">Reservas</A></LI>
 			</UL>
 		</NAV>
