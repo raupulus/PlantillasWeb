57518d21b774fa3995f913816a5f790ae69ca880
Fryntiz <fryntiz@gmail.com>
2017-02-16 10:07:39 +0100

    Renombrados enlaces de aside

--- Hostal/aside.php
+++ Hostal/aside.php
@@ -1,10 +1,9 @@
 			<ASIDE>
-				<H4>Menú de navegación</H4>
+				<H4>Consultas Rápidas</H4>
 				<UL>
-					<LI><A href="#" title="Link 1">link 1</A></LI>
-					<LI><A href="#" title="Link 1">link 2</A></LI>
-					<LI><A href="#" title="Link 3">link 3</A></LI>
-					<LI><A href="#" title="Link 4">link 4</A></LI>
+					<LI><A href="#" title="Link 1">Crear Cliente</A></LI>
+					<LI><A href="#" title="Link 1">Ver habitaciones libres</A></LI>
+					<LI><A href="#" title="Link 3">Crear Reserva</A></LI>
+					<LI><A href="#" title="Link 4">Eliminar Reserva</A></LI>
 				</UL>
-				<P>Descripción del apartado, datos, imagenes, banner, enlaces externos......</P>
 			</ASIDE>
