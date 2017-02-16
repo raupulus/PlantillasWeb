f52c03b33418d0cc0811f57c1cdfd52677c17d9a
Fryntiz <fryntiz@gmail.com>
2017-02-16 11:19:57 +0100

    Quitadas metaetiquetas para uniras en archivo general para todas las páginas

--- Hostal/index.php
+++ Hostal/index.php
@@ -5,19 +5,7 @@
 		<?php //Metaetiquetas globales importadas
 				include ('metaetiquetas-generales.php');
 			?>
-		<META name="keywords" content="administración, hotel, plantilla, web, fryntiz" /><!--Etiquetas-->
-		<META name="subject" content="Web para administrar Hotel" /><!--Descripción cortísima-->
-		<META name="description" content="" /><!--Breve resumen-->
 		<META name='revised' content='Tuesday, February 13th, 2017, 13:22 pm' /><!--Última revisión del sitio-->
-
-		<!--Metaetiquetas para mostrar WEB en Facebook-->
-		<META property='og:title' content='Titulo de la página' />
-		<META property='og:description' content='Descripción' />
-		<META property='og:image' content='http://www.dominioDelSitioWEB.es/img/logo.png' /><!--Miniatura que se mostrará junto al enlace-->
-
-		<!--Metaetiquetas para mostrar WEB en Twitter-->
-		<META name="twitter:title" content="Nombre de la publicación en twitter" />
-		<META name="twitter:description" content="Descripción de la página, máximo 200 caracteres" />
 	</HEAD>
 
 	<BODY>
