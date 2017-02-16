36ffb45fe4d6054b8d8831729011806188bf787e
Fryntiz <fryntiz@gmail.com>
2017-02-16 09:59:29 +0100

    eliminado aside de contenido.php en plantilla para independizarlo

--- 1-PLANTILLA_PHP/contenido.php
+++ 1-PLANTILLA_PHP/contenido.php
@@ -1,5 +1,3 @@
-		<DIV id="cajaContenido">
-			<DIV id="cajaEntradas">
 				<ARTICLE>
 					<HEADER>
 						<H2>Título de la Sección</H2>
@@ -36,17 +34,3 @@
 						<H3>Nombre del apartado 2 de la segunda sección</H3>
 						<P>Datos del apartado 2 (Relacionado con el 1 de la segunda sección)</P>
 					</SECTION>
-				</ARTICLE>
-			</DIV>
-
-			<ASIDE>
-				<H4>Menú de navegación</H4>
-				<UL>
-					<LI><A href="#" title="Link 1">link 1</A></LI>
-					<LI><A href="#" title="Link 1">link 2</A></LI>
-					<LI><A href="#" title="Link 3">link 3</A></LI>
-					<LI><A href="#" title="Link 4">link 4</A></LI>
-				</UL>
-				<P>Descripción del apartado, datos, imagenes, banner, enlaces externos......</P>
-			</ASIDE>
-		</DIV>
