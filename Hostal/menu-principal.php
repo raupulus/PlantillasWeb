4be8f19d2e2ad0b424de6ed1605e5837e85028a0
Fryntiz <fryntiz@gmail.com>
2017-02-16 13:34:18 +0100

    Enlace hacia habitaciones

--- Hostal/menu-principal.php
+++ Hostal/menu-principal.php
@@ -2,7 +2,7 @@
 			<UL>
 				<LI><A href="./index.php" title="Inicio">Inicio</A></LI>
 				<LI><A href="./clientes.php" title="Clientes">Clientes</A></LI>
-				<LI><A href="#" title="Habitaciones">Habitaciones</A></LI>
+				<LI><A href="./habitaciones.php" title="Habitaciones">Habitaciones</A></LI>
 				<LI><A href="#" title="Reservas">Reservas</A></LI>
 			</UL>
 		</NAV>
