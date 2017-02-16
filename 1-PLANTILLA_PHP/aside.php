0ad1e5e738ed7c5deb7292c38b3fbb20800eb5cd
Fryntiz <fryntiz@gmail.com>
2017-02-16 09:55:29 +0100

    Añadido contenido en aside.php de hotel

---
+++ 1-PLANTILLA_PHP/aside.php
@@ -0,0 +1,10 @@
+			<ASIDE>
+				<H4>Menú de navegación</H4>
+				<UL>
+					<LI><A href="#" title="Link 1">link 1</A></LI>
+					<LI><A href="#" title="Link 1">link 2</A></LI>
+					<LI><A href="#" title="Link 3">link 3</A></LI>
+					<LI><A href="#" title="Link 4">link 4</A></LI>
+				</UL>
+				<P>Descripción del apartado, datos, imagenes, banner, enlaces externos......</P>
+			</ASIDE>
