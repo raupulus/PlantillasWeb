2d5d7f2508ac0e24da33e6713ac5696838a86b74
Fryntiz <fryntiz@gmail.com>
2017-02-16 11:50:15 +0100

    Enlazado clientes.php desde menú aside

--- Hostal/menu-principal.php
+++ Hostal/menu-principal.php
@@ -1,8 +1,8 @@
 		<NAV id="cajaMenuPrincipal"><!--Menú principal bajo el título-->
 			<UL>
-				<LI><A href="#" title="Menú 1">Inicio</A></LI>
-				<LI><A href="#" title="Menú 2">Clientes</A></LI>
-				<LI><A href="#" title="Menú 3">Habitaciones</A></LI>
-				<LI><A href="#" title="Menú 4">Reservas</A></LI>
+				<LI><A href="./index.php" title="Inicio">Inicio</A></LI>
+				<LI><A href="./clientes.php" title="Clientes">Clientes</A></LI>
+				<LI><A href="#" title="Habitaciones">Habitaciones</A></LI>
+				<LI><A href="#" title="Reservas">Reservas</A></LI>
 			</UL>
 		</NAV>
