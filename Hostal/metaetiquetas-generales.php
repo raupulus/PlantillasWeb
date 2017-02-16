ba481569ef7e2fc39dc004b40949b397718a1291
Fryntiz <fryntiz@gmail.com>
2017-02-16 11:20:17 +0100

    añadidas metas que no se necesitan en cada página

--- Hostal/metaetiquetas-generales.php
+++ Hostal/metaetiquetas-generales.php
@@ -2,30 +2,32 @@
 		<META name="author" lang="es" content="Raúl Caro Pastorino, tecnico@fryntiz.es" />
 		<META name="organization" content="fryntiz" />
 		<META name='copyright' content='Nombre de la compañia/organización' />
-
+		<META name="keywords" content="administración, hotel, plantilla, web, fryntiz" /><!--Etiquetas-->
+		<META name="subject" content="Web para administrar Hotel" /><!--Descripción cortísima-->
+		<META name="description" content="" /><!--Breve resumen-->
 		<META name='language' content='ES' />
 		<META name="robots" content="index, follow" /> <!--Indica si es indexada (index|noindex) y si se siguen enlaces (follow|nofollow)-->
 		<META name="viewport" content="width=device-width, initial-scale=1.0" /><!--Web responsive, no zoom en smartphone. Si no es responsive css quitar-->
 		<META name="google-site-verification" content="????-->Código de verificación google WEBMASTER" /><!--Verifica mediante Search consle, webmaster tools de google-->
 
-
 		<!--Metaetiquetas para mostrar WEB en Facebook-->
 		<META property='fb:admins' content='????????' /> <!--ID de los usuarios admin del sitio-->
-
-
+		<META property='og:title' content='Titulo de la página' />
+		<META property='og:description' content='Descripción' />
+		<META property='og:image' content='http://www.dominioDelSitioWEB.es/img/logo.png' /><!--Miniatura que se mostrará junto al enlace-->
 		<META property="og:locale" content="es_ES" />
 		<META property="og:locale:alternate" content="es_LA" />
 		<META property='og:url' content='http://www.dominioDelSitioWEB.es/' /><!--Dominio del sitio-->
 		<META property='og:site_name' content='Nombre del sitio' />
 		<META property='og:type' content='website' /> <!--Normalmente website y article-->
 
-
 		<!--Metaetiquetas para mostrar WEB en Twitter-->
 		<META name="twitter:card" content="summary" /> <!--Define el tipo de tarjeta (summary|photo|player)-->
 		<META name="twitter:site" content="@Organization" /><!--@username de la pagina en twitter-->
 		<META name="twitter:creator" content="@Author" /> <!--@username del creador o autor del contenido-->
 		<META name='twitter:image' content='http://midominio.com/logo.png' />
-
+		<META name="twitter:title" content="Nombre de la publicación en twitter" />
+		<META name="twitter:description" content="Descripción de la página, máximo 200 caracteres" />
 
 		<!--Enlaces a favicon, CSS y JS-->
 		<LINK rel="shortcut icon" href="./img/favicon-32x32.png" />
