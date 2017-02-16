646245e234dd81166da57d8ef994bc7d71eca921
Fryntiz <fryntiz@gmail.com>
2017-02-16 12:06:28 +0100

    Añadida estructura de tabla para mostrar clientes

--- Hostal/clientes.php
+++ Hostal/clientes.php
@@ -12,12 +12,53 @@
 		<?php
 			include ('titulo.php');
 			include ('menu-principal.php');
-			echo	'<DIV id="cajaContenido">';
-			echo	'<DIV id="cajaEntradas">';
+			echo '<DIV id="cajaContenido">';
+			echo '<DIV id="cajaEntradas">';
+		?>
 
+		<ARTICLE>
+			<HEADER>
+				<H2>CLIENTES</H2>
+				<P>Detalles de clientes registrados</P>
+				<TIME datetime="2017-02-14T01:20:00+01:00"><SMALL>Último cliente registrado 14-02-2017 a las 01:20</SMALL></TIME>
+			</HEADER>
+
+			<SECTION>
+				<H3>Con reserva en este momento</H3>
+				<P>Clientes que tienen reserva</P>
+				<TABLE>
+					<TR>
+						<TD>Nombre</TD>
+						<TD>Teléfono</TD>
+						<TD>Ciudad</TD>
+						<TD>Nº Habitación</TD>
+					</TR>
+					<?php
+
+					?>
+				</TABLE>
+			</SECTION>
+
+			<SECTION>
+				<H3>Sin reserva en este momento</H3>
+				<P>Clientes que tienen no tienen reserva</P>
+				<TABLE>
+					<TR>
+						<TD>Nombre</TD>
+						<TD>Teléfono</TD>
+						<TD>Ciudad</TD>
+						<TD>Nº Habitación</TD>
+					</TR>
+					<?php
+
+					?>
+				</TABLE>
+			</SECTION>
+		</ARTICLE>
+
+		<?php
 			echo '</DIV>';
-			include ('aside.php');
-			echo	'</DIV>';
+			echo '</DIV>';
 			include ('footer.php');
 		?>
 	</BODY>
