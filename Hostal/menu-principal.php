81033a9a267e772923cba9d8e83294f251b9299e
Fryntiz <fryntiz@gmail.com>
2017-02-16 09:42:46 +0100

    Enlaces admin hotel

--- Hostal/menu-principal.php
+++ Hostal/menu-principal.php
@@ -1,8 +1,8 @@
 		<NAV id="cajaMenuPrincipal"><!--Menú principal bajo el título-->
 			<UL>
-				<LI><A href="#" title="Menú 1">Menú 1</A></LI>
-				<LI><A href="#" title="Menú 2">Menú 2</A></LI>
-				<LI><A href="#" title="Menú 3">Menú 3</A></LI>
+				<LI><A href="#" title="Menú 1">Inicio</A></LI>
+				<LI><A href="#" title="Menú 2">Clientes</A></LI>
+				<LI><A href="#" title="Menú 3">Habitaciones</A></LI>
 				<LI><A href="#" title="Menú 4">Menú 4</A></LI>
 			</UL>
 		</NAV>
