4a9ed1597f68d2a4b09bcfc6eccf27ebecbdb3f4
Fryntiz <fryntiz@gmail.com>
2017-03-18 21:03:14 +0100

    Estructura para datos de clientes con reservas actualizado

--- Hostal/clientes.php
+++ Hostal/clientes.php
@@ -35,8 +35,11 @@
 						<TH>Nombre</TH>
 						<TH>Apellidos</TH>
 						<TH>Teléfono</TH>
+						<TH>Provincia</TH>
 						<TH>Ciudad</TH>
 						<TH>Nº Habitación</TH>
+						<TH>Días</TH>
+						<TH>Precio</TH>
 					</TR>
 
 					<!--Temporal para diseño, esta parte se llena con la BD-->
@@ -45,28 +48,40 @@
 						<TD>Mero Calistro</TD>
 						<TD>612123123</TD>
 						<TD>Chipiona</TD>
+						<TD>Cádiz</TD>
 						<TD>13</TD>
+						<TD>2 días</TD>
+						<TD>112€</TD>
 					</TR>
 					<TR>
 						<TD>Ana</TD>
 						<TD>Corneta Quemada</TD>
 						<TD>6987987</TD>
 						<TD>Sevilla</TD>
+						<TD>Dos hermanas</TD>
 						<TD>11</TD>
+						<TD>4 días</TD>
+						<TD>192€</TD>
 					</TR>
 					<TR>
 						<TD>Pepe</TD>
 						<TD>Mero Calistro</TD>
 						<TD>612123123</TD>
 						<TD>Chipiona</TD>
+						<TD>Cádiz</TD>
 						<TD>13</TD>
+						<TD>8 días</TD>
+						<TD>392€</TD>
 					</TR>
 					<TR>
 						<TD>Ana</TD>
 						<TD>Corneta Quemada</TD>
 						<TD>6987987</TD>
 						<TD>Sevilla</TD>
+						<TD>Camas</TD>
 						<TD>11</TD>
+						<TD>5 días</TD>
+						<TD>292€</TD>
 					</TR>
 					<!--Fin de la parte temporal-->
 
