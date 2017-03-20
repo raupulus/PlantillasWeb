d1065765e4146145e8a6db9d5bab2542ba97cc3c
Fryntiz <fryntiz@gmail.com>
2017-03-20 11:27:17 +0100

    Enlaces para nuevas reservas y clientes añadidos en aside hotel

--- Hostal/aside.php
+++ Hostal/aside.php
@@ -1,8 +1,8 @@
 			<ASIDE>
 				<H4>Consultas Rápidas</H4>
 				<UL>
-					<LI><A href="#" title="Crear nuevo cliente">Crear Cliente</A></LI>
-					<LI><A href="#" title="Ver habitaciones libres">Ver habitaciones libres</A></LI>
+					<LI><A href="form-clientes.php" title="Crear nuevo cliente">Crear Cliente</A></LI>
+					<LI><A href="form-reservas.php" title="Ver habitaciones libres">Ver habitaciones libres</A></LI>
 					<LI><A href="#" title="Crear nueva reserva">Crear Reserva</A></LI>
 					<LI><A href="#" title="Eliminar Reserva">Eliminar Reserva</A></LI>
 					<LI><A href="#" title="Ver calendario">Ver calendario</A></LI>
