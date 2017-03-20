673128f6f2f8318743e3022132e76e6db661a6e6
Fryntiz <fryntiz@gmail.com>
2017-03-20 11:55:33 +0100

    Creada estructura marcando como será el formulario

--- Hostal/form-reservas.php
+++ Hostal/form-reservas.php
@@ -16,7 +16,7 @@
 			echo '<DIV id="cajaEntradas"><BR/>';
 		?>
 
-
+<!--Sería mejor separar esta parte y crear otra pagina donde pida seleccionar el usuario al que asignar la reserva primero y luego llevar a esta página donde se crea la reserva para el usuario elegido-->
 		<ARTICLE>
 			<HEADER>
 				<H2>Agregar Reserva</H2>
@@ -25,7 +25,44 @@
 			</HEADER>
 
 			<SECTION>
-				<H3>Nueva Reserva</H3>
+				<H3>Buscar cliente</H3>
+				<P>
+					Introduce algún dato del cliente para que aparezca en la lista
+				</P>
+
+				<DIV id="buscarCliente">
+					<P>Escribe un dato por el que buscar cliente:</P>
+					<BR />
+
+					<FORM>
+						Nombre<INPUT type="search" /> Apellidos <INPUT type="search" />
+						<BR /><BR />
+						Teléfono <INPUT type="search" /> DNI <INPUT type="search" />
+						<BR /><BR />
+						<INPUT type="button" value="Buscar" />
+					</FORM>
+				</DIV>
+			</SECTION>
+
+			<SECTION>
+				<H3>Seleccionar cliente</H3>
+				<P>
+					Elige el cliente al que asociar la reserva (tiene que estar creado previamente)
+				</P>
+
+				<FORM id="seleccionarCliente">
+					<BR/><BR/>
+					<SELECT>
+						<OPTION>cliente 1 Nombre, Apellidos - DNI - Telefono</OPTION>
+						<OPTION>cliente 2 Nombre, Apellidos - DNI - Telefono</OPTION>
+						<OPTION>cliente 3 Nombre, Apellidos - DNI - Telefono</OPTION>
+						<OPTION>cliente 4 Nombre, Apellidos - DNI - Telefono</OPTION>
+					</SELECT>
+				</FORM>
+			</SECTION>
+
+			<SECTION>
+				<H3>Datos de la nueva Reserva</H3>
 				<P>
 					Rellena los siguientes datos, los que tienen "*" son obligatorios
 				</P>
@@ -34,7 +71,6 @@
 					Nº Habitación <INPUT type="search" /><BR /><BR />
 					Fecha de Entrada <INPUT type="search" /><BR /><BR />
 					Fecha de Salida <INPUT type="search" /><BR /><BR />
-					DNI <INPUT type="search" /><BR /><BR />
 					Precio <INPUT type="search" /><BR /><BR />
 					Observación <INPUT type="textarea" /><BR /><BR />
 				</FORM>
