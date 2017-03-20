ece3ba4840774f5acf55dfa73483cf8ed3137761
Fryntiz <fryntiz@gmail.com>
2017-03-20 11:31:27 +0100

    Encabezados de sección reserva

--- Hostal/form-reservas.php
+++ Hostal/form-reservas.php
@@ -5,7 +5,7 @@
 		<?php //Metaetiquetas globales importadas
 			include ('metaetiquetas-generales.php');
 		?>
-		<META name='revised' content='Tuesday, February 20th, 2017, 11:29 pm' /><!--Última revisión del sitio-->
+		<META name='revised' content='Tuesday, February 20th, 2017, 11:08 pm' /><!--Última revisión del sitio-->
 	</HEAD>
 
 	<BODY>
@@ -16,6 +16,22 @@
 			echo '<DIV id="cajaEntradas"><BR/>';
 		?>
 
+
+		<ARTICLE>
+			<HEADER>
+				<H2>Agregar Reserva</H2>
+				<P>Formulario para agregar una nueva reserva</P>
+				<TIME datetime="2017-02-14T01:20:00+01:00"><SMALL>Última reserva añadida 14-02-2017 a las 12:10</SMALL></TIME>
+			</HEADER>
+
+			<SECTION>
+				<H3>Nueva Reserva</H3>
+				<P>
+					Rellena los siguientes datos, los que tienen "*" son obligatorios
+				</P>
+
+			</SECTION>
+		</ARTICLE>
 		<?php
 			echo '</DIV><BR/>';
 			echo '</DIV>';
