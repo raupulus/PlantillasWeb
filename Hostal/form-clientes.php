22fa8ecaa1e841d914633f2c1eb6434d17ce2624
Fryntiz <fryntiz@gmail.com>
2017-03-20 11:07:57 +0100

    incluida composición del sitio web

--- Hostal/form-clientes.php
+++ Hostal/form-clientes.php
@@ -1,10 +1,26 @@
 <!DOCTYPE html>
 <HTML lang="es">
 	<HEAD>
-
+		<TITLE>Administración Hotel</TITLE>
+		<?php //Metaetiquetas globales importadas
+			include ('metaetiquetas-generales.php');
+		?>
+		<META name='revised' content='Tuesday, February 20th, 2017, 11:08 pm' /><!--Última revisión del sitio-->
 	</HEAD>
 
 	<BODY>
+		<?php
+			include ('titulo.php');
+			include ('menu-principal.php');
+			echo '<DIV id="cajaContenido"><BR/>';
+			echo '<DIV id="cajaEntradas"><BR/>';
+		?>
 
+
+		<?php
+			echo '</DIV><BR/>';
+			echo '</DIV>';
+			include ('footer.php');
+		?>
 	</BODY>
 </HTML>
