62dc39e60058646629b5637a8128673de2683fac
Fryntiz <fryntiz@gmail.com>
2017-03-20 11:10:41 +0100

    Cabecera para sección cliente

--- Hostal/form-clientes.php
+++ Hostal/form-clientes.php
@@ -17,6 +17,14 @@
 		?>
 
 
+		<ARTICLE>
+			<HEADER>
+				<H2>Agregar Cliente</H2>
+				<P>Formulario para agregar un nuevo cliente</P>
+				<TIME datetime="2017-02-14T01:20:00+01:00"><SMALL>Último cliente añadido 14-02-2017 a las 01:20</SMALL></TIME>
+			</HEADER>
+
+		</ARTICLE>
 		<?php
 			echo '</DIV><BR/>';
 			echo '</DIV>';
