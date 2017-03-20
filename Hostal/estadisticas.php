b66507d5b1edd86eba01d42384c019a8cdfea5d0
Fryntiz <fryntiz@gmail.com>
2017-03-20 12:01:40 +0100

    incluido código básico del sitio

--- Hostal/estadisticas.php
+++ Hostal/estadisticas.php
@@ -1,10 +1,26 @@
 <!DOCTYPE html>
 <HTML lang="es">
 	<HEAD>
-
+		<TITLE>Administración Hotel</TITLE>
+		<?php //Metaetiquetas globales importadas
+				include ('metaetiquetas-generales.php');
+			?>
+		<META name='revised' content='Tuesday, February 20th, 2017, 13:10 pm' /><!--Última revisión del sitio-->
 	</HEAD>
 
 	<BODY>
+		<?php
+			include ('titulo.php');
+			include ('menu-principal.php');
+			echo	'<DIV id="cajaContenido">';
+			echo	'<DIV id="cajaEntradas">';
 
+
+
+			echo '</DIV>';
+			include ('aside.php');
+			echo	'</DIV>';
+			include ('footer.php');
+		?>
 	</BODY>
 </HTML>
