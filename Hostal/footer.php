553f2ac9a8ce1c9d3eb74ecbae512e41a3c7525e
Fryntiz <fryntiz@gmail.com>
2017-03-23 22:05:45 +0100

    nombre marcando enlaces

--- Hostal/footer.php
+++ Hostal/footer.php
@@ -5,8 +5,8 @@
 					<UL>
 						<LI><A href="#" title="Link 1">Enlace 1</A></LI>
 						<LI><A href="#" title="Link 2">Enlace 2</A></LI>
-						<LI><A href="#" title="Link 1">Enlace 1</A></LI>
-						<LI><A href="#" title="Link 2">Enlace 2</A></LI>
+						<LI><A href="#" title="Link 3">Enlace 3</A></LI>
+						<LI><A href="#" title="Link 4">Enlace 4</A></LI>
 					</UL>
 				</NAV>
 
@@ -14,8 +14,8 @@
 					<UL>
 						<LI><A href="#" title="Link 1">Enlace 1</A></LI>
 						<LI><A href="#" title="Link 2">Enlace 2</A></LI>
-						<LI><A href="#" title="Link 1">Enlace 1</A></LI>
-						<LI><A href="#" title="Link 2">Enlace 2</A></LI>
+						<LI><A href="#" title="Link 3">Enlace 3</A></LI>
+						<LI><A href="#" title="Link 4">Enlace 4</A></LI>
 					</UL>
 				</NAV>
 
@@ -23,8 +23,8 @@
 					<UL>
 						<LI><A href="#" title="Link 1">Enlace 1</A></LI>
 						<LI><A href="#" title="Link 2">Enlace 2</A></LI>
-						<LI><A href="#" title="Link 1">Enlace 1</A></LI>
-						<LI><A href="#" title="Link 2">Enlace 2</A></LI>
+						<LI><A href="#" title="Link 3">Enlace 3</A></LI>
+						<LI><A href="#" title="Link 4">Enlace 4</A></LI>
 					</UL>
 				</NAV>
 			</DIV>
